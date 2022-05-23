257a4734e301a0b8e54ae1ff4ec01ff76805de20
mateodelcamp <mateodelcampillo01@gmail.com>
2022-05-23 00:19:56 -0300

    git testing

--- resultado.js
+++ resultado.js
@@ -1,36 +1,56 @@
 uno = document.querySelector(".uno").addEventListener("click", ()=>{op = 1
 pushOp()
 n1.value += op
+n2.value = ""
 console.log(arrayResultado)})
 dos = document.querySelector(".dos").addEventListener("click", ()=>{op = 2
    pushOp()
-   n1.value += op})
+   n1.value += op
+   n2.value = ""})
 tres = document.querySelector(".tres").addEventListener("click", ()=>{op = 3
    pushOp()
-   n1.value += op})
+   n1.value += op
+   n2.value = ""})
 cuatro = document.querySelector(".cuatro").addEventListener("click", ()=>{op = 4
    pushOp()
-   n1.value += op})
+   n1.value += op
+   n2.value = ""})
 cinco = document.querySelector(".cinco").addEventListener("click", ()=>{op = 5
    pushOp()
-   n1.value += op})
+   n1.value += op
+   n2.value = ""})
 seis = document.querySelector(".seis").addEventListener("click", ()=>{op = 6
    pushOp()
-   n1.value += op})
+   n1.value += op
+   n2.value = ""})
 siete = document.querySelector(".siete").addEventListener("click", ()=>{op = 7
    pushOp()
-   n1.value += op})
+   n1.value += op
+   n2.value = ""})
 ocho = document.querySelector(".ocho").addEventListener("click", ()=>{op = 8
    pushOp()
-   n1.value += op})
+   n1.value += op
+   n2.value = ""})
 nueve = document.querySelector(".nueve").addEventListener("click", ()=>{op = 9
    pushOp()
-   n1.value += op})
+   n1.value += op
+   n2.value = ""})
 cero = document.querySelector(".cero").addEventListener("click", ()=>{op = 0
    pushOp()
-   n1.value += op})
+   n1.value += op
+   n2.value = ""})
+parentesis1 = document.querySelector(".parentesis1").addEventListener("click", ()=>{op = "("
+   pushOp()
+   n1.value += op
+   n2.value = ""})
+parentesis2 = document.querySelector(".parentesis2").addEventListener("click", ()=>{op = ")"
+   pushOp()
+   n1.value += op
+   n2.value = ""})      
 n1 = document.querySelector(".n1")
+n2 = document.querySelector(".n2")
 n1.value = ""
+n2.value = ""
 // // Array almacenador de resultados
 // uno.addEventListener("click", ()=>{
 //    arrayResultado.push(uno.value)
@@ -88,12 +108,18 @@ n1.value += op})
 document.querySelector(".division").addEventListener("click", ()=> {op = '/'
 pushOp()
 n1.value += op})
+document.querySelector(".back").addEventListener("click", ()=> {
+opToString.slice()
+})
+
 // 
 igual = document.querySelector(".igual").addEventListener("click", ()=>{
   opToString = arrayResultado.join("")
   resultado = eval(opToString)
-  n1.value = resultado
-   arrayResultado = []
+  n1.value = ""
+  n2.value = resultado
+  arrayResultado = []
+   // arrayResultado = []
    console.log(resultado)})
 // // Evento para resetear el resultado y realizar una nueva operacion
 // document.querySelector(".reset").addEventListener("click", ()=> {
